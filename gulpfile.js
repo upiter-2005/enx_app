9ecead11ac2454b143f28df8297d44d5358ec9e9
upiter-2005 <kuchenyov-pavel@mail.ru>
2023-04-26 18:34:58 +0300

    styles comment

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,14 @@ function styles() {
     .pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
     .pipe(concat("main.min.css")) // Конкатенируем в файл main.min.js
     .pipe(autoprefixer({ overrideBrowserslist: ["last 10 versions"], grid: true })) // Создадим префиксы с помощью Autoprefixer
-    .pipe(cleancss({ level: { 1: { specialComments: 0 } } /* , format: 'beautify' */ })) // Минифицируем стили
+    .pipe(
+      cleancss({
+        level: {
+          1: { specialComments: "all", sourceMap: true },
+        },
+        format: "beautify",
+      }),
+    ) // Минифицируем стили
     .pipe(dest("app/css/")) // Выгрузим результат в папку "app/css/"
     .pipe(browserSync.stream()); // Сделаем инъекцию в браузер
 }
